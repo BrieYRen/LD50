4cb86332bf2fb5255b4d6c0f1371af14bdaae29d
BrieYRen <brieyr@outlook.com>
2022-04-05 15:31:31 +0800

    Update place item feature to detect if mouse position is within the free placement area

--- LD50/Assets/Scripts/Interact/PlaceItem.cs
+++ LD50/Assets/Scripts/Interact/PlaceItem.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
 /// <summary>
@@ -20,11 +21,17 @@ public class PlaceItem : MonoBehaviour
 
     public RectTransform itemToSpawn;
 
+    public CanvasGroup freeplaceParent;
+    public string areaTag;
+
 
     private void Start()
     {
         cursorSwitcher = GameManager.instance.cursorSwitcher;
         inventory = GameManager.instance.inventoryManager;
+
+        freeplaceParent.blocksRaycasts = false;
+        freeplaceParent.alpha = 0f;
 
         SceneManager.sceneUnloaded += OnSceneUnloaded;
     }
@@ -39,6 +46,9 @@ public class PlaceItem : MonoBehaviour
     {
         canPlace = true;
 
+        freeplaceParent.blocksRaycasts = true;
+        freeplaceParent.alpha = 1f;
+
         cursorSwitcher.SetToCursor(item.placeCursor, item.placeCursorOffset);
     }
 
@@ -46,24 +56,51 @@ public class PlaceItem : MonoBehaviour
     {        
         canPlace = false;
 
+        freeplaceParent.blocksRaycasts = false;
+        freeplaceParent.alpha = 0f;
+
         cursorSwitcher.SetNormal();
     }
 
     public void OnClickPlaceButton()
     {
-        // spawn an interactable object at mouse position      
-        Vector2 mousePos = new Vector2();
-        RectTransformUtility.ScreenPointToLocalPointInRectangle(toPlaceCanvas.transform as RectTransform, Input.mousePosition, toPlaceCanvas.worldCamera, out mousePos);
+        // check if the click position is within the free place area
+        bool canFreePlace = false;
 
-        itemToSpawn.transform.position = toPlaceCanvas.transform.TransformPoint(mousePos);
-        itemToSpawn.gameObject.SetActive(true);
+        PointerEventData pointerEventData = new PointerEventData(EventSystem.current);
+        pointerEventData.position = Input.mousePosition;
 
-        // remove from inventory 
-        inventory.Removed(item);
+        List<RaycastResult> raycastResults = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(pointerEventData, raycastResults);
+
+        if (raycastResults.Count > 0)
+        {
+            for (int i = 0; i < raycastResults.Count; i++)
+            {
+                if (raycastResults[i].gameObject.CompareTag(areaTag))
+                    canFreePlace = true;
+            }
+        }
+
+        // if it's in free place area   
+        if (canFreePlace)
+        {
+            // place the interactable object at mouse position 
+            Vector2 mousePos = new Vector2();
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(toPlaceCanvas.transform as RectTransform, Input.mousePosition, toPlaceCanvas.worldCamera, out mousePos);
+
+            itemToSpawn.transform.position = toPlaceCanvas.transform.TransformPoint(mousePos);
+            itemToSpawn.gameObject.SetActive(true);
+
+            // remove from inventory 
+            inventory.Removed(item);
+        }
+        
 
         // leave the ready to place state
         BackNormal();
     }
+
 
     // destroy the spawned items on level change
     void OnSceneUnloaded(Scene scene)
